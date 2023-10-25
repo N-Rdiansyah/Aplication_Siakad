4ad717c7623fc67be21df3bf53f8a036db542d6f
unknown <rdiansyah.muhammad@gmail.com>
2023-10-25 21:29:20 +0700

    Menambah Authentifikasi

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -226,7 +226,6 @@
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST">
                     @csrf
-                    <button type="submit">Logout</button>
                 </form>
 
 
